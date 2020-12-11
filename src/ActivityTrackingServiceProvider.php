fc24a2ed5dc30550002e4555e231fcedf7be8919
vshoholev <vshoholev@fincue.com>
2020-12-11 13:06:12 +0200

    master migration load fix

--- src/ActivityTrackingServiceProvider.php
+++ src/ActivityTrackingServiceProvider.php
@@ -4,6 +4,8 @@
 namespace ElfR\ActivityTrack;
 
 
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 /**
@@ -32,4 +34,20 @@ class ActivityTrackingServiceProvider extends ServiceProvider
         parent::register();
     }
 
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @param Filesystem $filesystem
+     * @return string
+     */
+    protected function getMigrationFileName(Filesystem $filesystem): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path.'*_create_activity_tracks_table.php');
+            })->push($this->app->databasePath()."/migrations/{$timestamp}_create_activity_tracks_table.php")
+            ->first();
+    }
 }
